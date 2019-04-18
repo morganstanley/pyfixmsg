7e553fc5b539d4a0852416837ad579eedb3eb85f
franso6 <francois-github@flve.uk>
2019-04-18 18:33:41 +0200

    restore unexpected deleted test case
    restore unexpected deleted test case

--- tests/test_ref.py
+++ tests/test_ref.py
@@ -270,6 +270,35 @@ class TestReference(object):
                      '688=2;689=2;17807=11;10=000;'.replace(';', chr(1)).encode('UTF-8')
         assert serialised == codec.serialise(msg)
 
+    def test_empty_rgroups(self, spec):
+        codec = Codec(spec=spec, decode_as='UTF-8')
+        msg = b'35=AJ;17807=11;232=2;233=bli;234=blu;' \
+              b'233=blih;234=bluh;555=0;10=000;'
+        msg = codec.parse(msg, separator=';')
+        assert {35: 'AJ',
+                17807: '11',
+                232: [
+                    {233: 'bli', 234: 'blu'},
+                    {233: 'blih', 234: 'bluh'}
+                ],
+                555: [],
+                10: '000'
+                } == msg
+        lhs = tuple(codec._unmap(msg))
+        assert lhs == ((35, 'AJ'),
+                       (232, 2),
+                       (233, 'bli'),
+                       (234, 'blu'),
+                       (233, 'blih'),
+                       (234, 'bluh'),
+                       (555, 0),
+                       (17807, '11'),
+                       (10, '000')
+                       )
+        serialised = '35=AJ;232=2;233=bli;234=blu;233=blih;234=bluh;555=0;' \
+                     '17807=11;10=000;'.replace(';', chr(1)).encode('UTF-8')
+        assert serialised == codec.serialise(msg)
+
     def test_large_msg(self, spec, profiler):
         setup = """
 import pyfixmsg.reference as ref
